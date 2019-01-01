b8e7a84ab2b8b6b6b2a229b1d965506ef9268c36
BrabaraM <barbara.mrugalska@gmail.com>
2019-01-01 20:50:02 +0100

    10 - implement FileHelper class. Added bodies to junit tests, changed writeLine method.

--- src/main/java/pl/coderstrust/repository/FileHelper.java
+++ src/main/java/pl/coderstrust/repository/FileHelper.java
@@ -1,7 +1,9 @@
 package pl.coderstrust.repository;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -13,7 +15,7 @@ public class FileHelper {
   private File file = null;
 
   public void create(String filePath) throws IOException {
-    if (filePath == null){
+    if (filePath == null) {
       throw new IllegalArgumentException("filePath cannot be null.");
     }
     if (file == null) {
@@ -23,7 +25,7 @@ public class FileHelper {
   }
 
   public void delete(String filePath) throws IOException {
-    if (filePath == null){
+    if (filePath == null) {
       throw new IllegalArgumentException("filePath cannot be null.");
     }
     create(filePath);
@@ -39,27 +41,28 @@ public class FileHelper {
   }
 
   public void clear(String filePath) throws FileNotFoundException {
-    if (filePath == null){
+    if (filePath == null) {
       throw new IllegalArgumentException("filePath cannot be null.");
     }
     PrintWriter printWriter = new PrintWriter(new File(filePath));
     printWriter.close();
   }
 
-  public void writeLine(String filePath, String line) throws FileNotFoundException {
-    if (filePath == null){
+  public void writeLine(String filePath, String line) throws IOException {
+    if (filePath == null) {
       throw new IllegalArgumentException("filePath cannot be null.");
     }
-    if (line == null){
+    if (line == null) {
       throw new IllegalArgumentException("line cannot be null.");
     }
-    PrintWriter printWriter = new PrintWriter(filePath);
-    printWriter.println(line);
-    printWriter.close();
+    BufferedWriter writer = new BufferedWriter(new FileWriter(new File(filePath), true));
+    writer.append(line);
+    writer.newLine();
+    writer.close();
   }
 
   public List<String> readLines(String filePath) throws FileNotFoundException {
-    if (filePath == null){
+    if (filePath == null) {
       throw new IllegalArgumentException("filePath cannot be null.");
     }
     List<String> lines = new ArrayList<>();
@@ -72,7 +75,7 @@ public class FileHelper {
   }
 
   public String readLastLine(String filePath) throws FileNotFoundException {
-    if (filePath == null){
+    if (filePath == null) {
       throw new IllegalArgumentException("filePath cannot be null.");
     }
     Scanner scanner = new Scanner(new File(filePath));
@@ -85,8 +88,8 @@ public class FileHelper {
   }
 
   public void removeLine(int lineNumber) throws FileNotFoundException {
-    if (lineNumber >= 0){
-      throw new IllegalArgumentException("lineNumber cannot less or equal to zero.");
+    if (lineNumber <= 0) {
+      throw new IllegalArgumentException("lineNumber cannot be less or equal to zero.");
     }
     List<String> lines = readLines(file.getPath());
     lines.remove(lineNumber - 1);
--- src/test/java/pl/coderstrust/repository/FileHelperTest.java
+++ src/test/java/pl/coderstrust/repository/FileHelperTest.java
@@ -1,44 +1,143 @@
 package pl.coderstrust.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import junit.framework.Assert;
 import org.junit.jupiter.api.Test;
 
 class FileHelperTest {
 
+  private FileHelper fileHelper = new FileHelper();
+
   @Test
-  void shouldCreateNewFile() {
+  void checkIfCreateNewFile() throws IOException {
+    //Given
+    String filePath = "src/test/test resources/file1.txt";
+
+    //When
+    fileHelper.create(filePath);
+    boolean isCreated = fileHelper.exists();
+
+    //Then
+    Assert.assertTrue(isCreated);
   }
 
   @Test
-  void shouldDeleteFile() {
+  void checkIfDeleteFile() throws IOException {
+    //Given
+    String filePath = "src/test/test resources/file2.txt";
+
+    //When
+    fileHelper.delete(filePath);
+    boolean isCreated = fileHelper.exists();
+
+    //Then
+    Assert.assertFalse(isCreated);
   }
 
   @Test
-  void checkIfFileExists() {
+  void checkIfFileExists() throws IOException {
+    //Given
+    String filePath = "src/test/test resources/file3.txt";
+
+    //When
+    fileHelper.create(filePath);
+    boolean isCreated = fileHelper.exists();
+
+    //Then
+    Assert.assertTrue(isCreated);
   }
 
   @Test
-  void checkIfFileIsEmpty() {
+  void checkIfFileIsEmpty() throws IOException {
+    //Given
+    String filePath = "src/test/test resources/file4.txt";
+    fileHelper.create(filePath);
+
+    //When
+    boolean isEmpty = fileHelper.isEmpty();
+
+    //Then
+    Assert.assertTrue(isEmpty);
   }
 
   @Test
-  void shouldClearFile() {
+  void checkIfClearFile() throws IOException {
+    //Given
+    String filePath = "src/test/test resources/file5.txt";
+    fileHelper.create(filePath);
+    fileHelper.writeLine(filePath, "file is not empty");
+
+    //When
+    fileHelper.clear(filePath);
+    boolean isEmpty = fileHelper.isEmpty();
+
+    //Then
+    Assert.assertTrue(isEmpty);
   }
 
   @Test
-  void shouldWriteLineToFile() {
+  void checkIfWriteLineToFile() throws IOException {
+    //Given
+    String filePath = "src/test/test resources/file6.txt";
+    fileHelper.create(filePath);
+
+    //When
+    fileHelper.writeLine(filePath, "file is not empty");
+    String line = fileHelper.readLastLine(filePath);
+
+    //Then
+    Assert.assertEquals("file is not empty", line);
+
   }
 
   @Test
-  void shouldReadLinesFromFile() {
+  void checkIfReadLinesFromFile() throws IOException {
+    //Given
+    String filePath = "src/test/test resources/file7.txt";
+    List<String> expected = new ArrayList<>();
+    expected.add("first line");
+    expected.add("second line");
+    expected.add("third line");
+
+    //When
+    List<String> actual = fileHelper.readLines(filePath);
+
+    //Then
+    Assert.assertEquals(expected.toString(), actual.toString());
   }
 
   @Test
-  void shouldReadLastLineFromFile() {
+  void checkIfReadLastLineFromFile() throws FileNotFoundException {
+    //Given
+    String filePath = "src/test/test resources/file8.txt";
+    String expected = "third line";
+
+    //When
+    String actual = fileHelper.readLastLine(filePath);
+
+    //Then
+    Assert.assertEquals(expected, actual);
   }
 
   @Test
-  void shouldRemoveLineFromFile() {
+  void checkIfRemoveLineFromFile() throws IOException {
+    //Given
+    String filePath = "src/test/test resources/file9.txt";
+    fileHelper.clear(filePath);
+    fileHelper.writeLine(filePath, "first line");
+    fileHelper.writeLine(filePath, "second line");
+    fileHelper.writeLine(filePath, "third line");
+    String expected = "second line";
+
+    //When
+    fileHelper.create(filePath);
+    fileHelper.removeLine(3);
+    String actual = fileHelper.readLastLine(filePath);
+
+    //Then
+    Assert.assertEquals(expected, actual);
   }
 }
