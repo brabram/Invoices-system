d18b671ba32735a77f1021d6eaa66754659eb704
BrabaraM <barbara.mrugalska@gmail.com>
2019-01-07 23:45:20 +0100

    #15 Create InvoiceBook class. changes in InvoiceBook class, removed 'update' method to InvoiceRepository.

---
+++ src/main/java/pl/coderstrust/logic/InvoiceBook.java
@@ -0,0 +1,54 @@
+package pl.coderstrust.logic;
+
+import java.time.LocalDate;
+import java.util.List;
+import pl.coderstrust.model.Invoice;
+import pl.coderstrust.repository.InvoiceRepository;
+import pl.coderstrust.repository.InvoiceRepositoryOperationException;
+
+public class InvoiceBook {
+
+  private InvoiceRepository invoiceRepository;
+  private Invoice invoice;
+
+  public InvoiceBook(InvoiceRepository invoiceRepository) {
+    this.invoiceRepository = invoiceRepository;
+  }
+
+  public List<Invoice> getAllInvoices() throws InvoiceRepositoryOperationException {
+    return invoiceRepository.findAll();
+  }
+
+  public List<Invoice> getAllInvoicesInGivenDateRange(LocalDate fromDate, LocalDate toDate) throws InvoiceRepositoryOperationException {
+    if (invoice.getIssueDate().isAfter(fromDate) && invoice.getIssueDate().isBefore(toDate)) {
+      return invoiceRepository.findAll();
+    }
+    return null;
+  }
+
+  public Invoice getInvoiceById(Integer id) throws InvoiceRepositoryOperationException {
+    return (Invoice) invoiceRepository.findById(id);
+  }
+
+  public Invoice addInvoice(Invoice invoice) throws InvoiceRepositoryOperationException {
+    return (Invoice) invoiceRepository.save(invoice);
+  }
+
+  public void updateInvoice(Invoice invoice) throws InvoiceRepositoryOperationException {
+    invoiceRepository.save(invoice);
+  }
+
+  public void deleteInvoiceById(Integer id) throws InvoiceRepositoryOperationException {
+    invoiceRepository.deleteById(id);
+  }
+
+  public void deleteInvoice(Invoice invoice) throws InvoiceRepositoryOperationException {
+    if (invoiceRepository.existsById(invoice.getId())) {
+      deleteInvoiceById(invoice.getId());
+    }
+  }
+
+  public void deleteAll() throws InvoiceRepositoryOperationException {
+    invoiceRepository.deleteAll();
+  }
+}
