152f3c191160070658d3e4bb5c6679e91a22034d
git@github.com:CodersTrustPL/solutions-8-maksym.git <chernevskyymaks@gmail.com>
2019-01-08 22:01:41 +0100

    14 Validators

--- src/main/java/pl/coderstrust/validators/AccountNumberValidator.java
+++ src/main/java/pl/coderstrust/validators/AccountNumberValidator.java
@@ -3,21 +3,64 @@ package pl.coderstrust.validators;
 import pl.coderstrust.model.AccountNumber;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class AccountNumberValidator {
-  public static final int LOCAL_ACCOUNT_NUMBER_SIZE = 24;
-
-  public static List<String> validate(AccountNumber accountNumber) {
-    List<String> result = new ArrayList();
-    if (accountNumber.getLocalNumber().length() > LOCAL_ACCOUNT_NUMBER_SIZE || accountNumber.getLocalNumber().length() < LOCAL_ACCOUNT_NUMBER_SIZE) {
-      throw new IllegalArgumentException("Incorrect local number size");
+  public static List<String> validate(AccountNumber accountNumber) {//czy nie jest null
+    if(accountNumber == null){
+      return Collections.singletonList("Account number cannot be null");
     }
-   String localNumber = accountNumber.getLocalNumber();
-   localNumber.format("%d", "[0-9]{24}");
-   String ibanNumber = String.format("PL%d" , localNumber);
-   result.add(localNumber);
-   result.add(ibanNumber);
+
+    List<String> result = new ArrayList();
+
+    String ibanNumberValidation = validateIbanNumber(accountNumber.getIbanNumber());
+    String localNumberValidation = validateLocalNumber(accountNumber.getLocalNumber());
+    String compare = compareNumbers(accountNumber.getLocalNumber(), accountNumber.getIbanNumber());
+
+    addResultOfValidation(result, ibanNumberValidation);
+    addResultOfValidation(result, localNumberValidation);
+    addResultOfValidation(result, compare);
+
    return result;
+  }
+
+  private static String validateLocalNumber(String localNumber){
+    if (localNumber == null){
+      return "Local number cannot be null.";
+    }
+    if(localNumber.trim().isEmpty()){
+      return "Local number cannot be empty.";
+    }
+    if (!localNumber.matches("[09]+")){
+      return "Local number cannot contain letters or another special chars.";
+    }
+    return "";
+  }
+
+  private static String validateIbanNumber(String ibanNumber){
+    if (ibanNumber == null){
+      return "Iban number cannot be null.";
+    }
+    if(ibanNumber.trim().isEmpty()){
+      return "Iban number cannot be empty.";
+    }
+    if (!ibanNumber.matches("[AZ]{2}[09]+")){
+      return "PL123556";
+    }
+    return "";
+  }
+
+  private static String compareNumbers(String localNumber, String ibanNumber) {
+    if (localNumber != ibanNumber.substring(3)) {
+      return "sdfsdf";
+    }
+    return "";
+  }
+
+  private static void addResultOfValidation(List<String> resultList, String resultOfValidation) {
+    if (resultOfValidation != null && !resultOfValidation.trim().isEmpty()){
+      resultList.add(resultOfValidation);
+    }
   }
 }
