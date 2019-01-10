b7ff82679fc1b619ecd75494f1a21c87d0d32b34
BrabaraM <barbara.mrugalska@gmail.com>
2019-01-10 16:36:06 +0100

    #15 Create InvoiceBook class. Added unit tests.

--- src/test/java/pl/coderstrust/logic/InvoiceBookTest.java
+++ src/test/java/pl/coderstrust/logic/InvoiceBookTest.java
@@ -1,9 +1,135 @@
 package pl.coderstrust.logic;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-class InvoiceBookTest {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import pl.coderstrust.model.Invoice;
+import pl.coderstrust.repository.InvoiceRepository;
+import pl.coderstrust.repository.InvoiceRepositoryOperationException;
 
+@RunWith(MockitoJUnitRunner.class)
+public class InvoiceBookTest {
 
+  @Mock
+  private InvoiceRepository<Invoice, Integer> invoiceRepository;
 
+  @Mock
+  private Invoice invoice;
+
+  @InjectMocks
+  private InvoiceBook invoiceBook;
+
+  @Test
+  public void shouldGetAllInvoices() throws InvoiceRepositoryOperationException {
+    //Given
+    List<Invoice> invoices = new ArrayList<>();
+    when(invoiceRepository.findAll()).thenReturn(invoices);
+
+    //When
+    invoiceBook.getAllInvoices();
+
+    //Then
+    verify(invoiceRepository).findAll();
+  }
+
+  @Test
+  public void shouldGetAllInvoicesInGivenDateRange() throws InvoiceRepositoryOperationException {
+    //Given
+    LocalDate fromDate = LocalDate.parse("2019-01-01");
+    LocalDate toDate = LocalDate.parse("2019-01-10");
+    List<Invoice> invoices = new ArrayList<>();
+//    when(invoice.getIssueDate()).thenReturn(fromDate);
+//    when(invoice.getIssueDate()).thenReturn(toDate);
+    when(invoiceRepository.findAll()).thenReturn(invoices);
+
+    //When
+    invoiceBook.getAllInvoicesInGivenDateRange(fromDate, toDate);
+
+    //Then
+    verify(invoiceRepository).findAll();
+  }
+
+  @Test
+  public void shouldGetInvoiceById() throws InvoiceRepositoryOperationException {
+    //Given
+    Integer id = 0;
+    when(invoiceRepository.findById(id)).thenReturn(invoice);
+
+    //When
+    invoiceBook.getInvoiceById(id);
+
+    //Then
+    verify(invoiceRepository).findById(id);
+  }
+
+  @Test
+  public void shouldAddInvoice() throws InvoiceRepositoryOperationException {
+    //Given
+    when(invoiceRepository.save(invoice)).thenReturn(invoice);
+
+    //When
+    invoiceBook.addInvoice(invoice);
+
+    //Then
+    verify(invoiceRepository).save(invoice);
+  }
+
+  @Test
+  public void shouldUpdateInvoice() throws InvoiceRepositoryOperationException {
+    //Given
+    when(invoiceRepository.save(invoice)).thenReturn(invoice);
+
+    //When
+    invoiceBook.updateInvoice(invoice);
+
+    //Then
+    verify(invoiceRepository).save(invoice);
+  }
+
+  @Test
+  public void shouldDeleteInvoiceById() throws InvoiceRepositoryOperationException {
+    //Given
+    Integer id = 0;
+    doNothing().when(invoiceRepository).deleteById(id);
+
+    //When
+    invoiceBook.deleteInvoiceById(id);
+
+    //Then
+    verify(invoiceRepository).deleteById(id);
+  }
+
+  @Test
+  public void shouldDeleteInvoice() throws InvoiceRepositoryOperationException {
+    //Given
+    Integer id = 0;
+    doNothing().when(invoiceRepository).deleteById(id);
+
+    //When
+    invoiceBook.deleteInvoice(invoice);
+
+    //Then
+    verify(invoiceRepository).deleteById(id);
+  }
+
+  @Test
+  public void shouldDeleteAll() throws InvoiceRepositoryOperationException {
+    //Given
+    doNothing().when(invoiceRepository).deleteAll();
+
+    //When
+    invoiceBook.deleteAll();
+
+    //Then
+    verify(invoiceRepository).deleteAll();
+  }
 }
