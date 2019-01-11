435d43688774e4abb59f711071ac4605bcc633f5
git@github.com:CodersTrustPL/solutions-8-maksym.git <chernevskyymaks@gmail.com>
2019-01-11 01:08:49 +0100

    9inMemotyInvoiceRepository

--- src/test/java/pl/coderstrust/repository/InMemoryInvoiceRepositoryTest.java
+++ src/test/java/pl/coderstrust/repository/InMemoryInvoiceRepositoryTest.java
@@ -153,10 +153,10 @@ class InMemoryInvoiceRepositoryTest {
     Invoice invoiceToSave1 = InvoiceGenerator.getRandomInvoice();
     Invoice savedInvoice1 = invoiceRepository.save(invoiceToSave1);
     Invoice invoiceToSave2 = InvoiceGenerator.getRandomInvoice();
-    Invoice savedInvoice2 = invoiceRepository.save(invoiceToSave2);
+    Invoice savedInvoice3 = invoiceRepository.save(invoiceToSave2);
     List<Invoice> invoicesInRepository = new ArrayList<>();
     invoicesInRepository.add(savedInvoice1);
-    invoicesInRepository.add(savedInvoice2);
+    invoicesInRepository.add(savedInvoice3);
     Assert.assertEquals(invoicesInRepository.size(), invoiceRepository.count());
     //when
     invoiceRepository.deleteAll();
