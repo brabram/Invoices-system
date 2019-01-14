8a85164ac7230c5be3c2d248e1fc7e1560aa5a6b
BrabaraM <barbara.mrugalska@gmail.com>
2019-01-14 13:53:06 +0100

    #13 Change type of id in model classes to Strng.

--- src/main/java/pl/coderstrust/model/InvoiceEntry.java
+++ src/main/java/pl/coderstrust/model/InvoiceEntry.java
@@ -4,31 +4,32 @@ import java.math.BigDecimal;
 import java.util.Objects;
 
 public class InvoiceEntry {
-    private String id;
-    private String item;
-    private Long quantity;
-    private BigDecimal price;
-    private BigDecimal vatValue;
-    private BigDecimal grossValue;
-    private Vat vatRate;
 
-    public InvoiceEntry(String id, String item, Long quantity, BigDecimal price, BigDecimal vatValue, BigDecimal grossValue, Vat vatRate) {
-        this.id = id;
-        this.item = item;
-        this.quantity = quantity;
-        this.price = price;
-        this.vatValue = vatValue;
-        this.grossValue = grossValue;
-        this.vatRate = vatRate;
-    }
+  private String id;
+  private String item;
+  private Long quantity;
+  private BigDecimal price;
+  private BigDecimal vatValue;
+  private BigDecimal grossValue;
+  private Vat vatRate;
 
-    public String getId() {
-        return id;
-    }
+  public InvoiceEntry(String id, String item, Long quantity, BigDecimal price, BigDecimal vatValue, BigDecimal grossValue, Vat vatRate) {
+    this.id = id;
+    this.item = item;
+    this.quantity = quantity;
+    this.price = price;
+    this.vatValue = vatValue;
+    this.grossValue = grossValue;
+    this.vatRate = vatRate;
+  }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
 
   public String getItem() {
     return item;
@@ -78,32 +79,32 @@ public class InvoiceEntry {
     this.vatRate = vatRate;
   }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        InvoiceEntry invoiceEntry = (InvoiceEntry) o;
-        return Objects.equals(id, invoiceEntry.id) &&
-                Objects.equals(item, invoiceEntry.item) &&
-                Objects.equals(quantity, invoiceEntry.quantity) &&
-                Objects.equals(price, invoiceEntry.price) &&
-                Objects.equals(vatValue, invoiceEntry.vatValue) &&
-                Objects.equals(grossValue, invoiceEntry.grossValue) &&
-                vatRate == invoiceEntry.vatRate;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    InvoiceEntry invoiceEntry = (InvoiceEntry) o;
+    return Objects.equals(id, invoiceEntry.id) &&
+        Objects.equals(item, invoiceEntry.item) &&
+        Objects.equals(quantity, invoiceEntry.quantity) &&
+        Objects.equals(price, invoiceEntry.price) &&
+        Objects.equals(vatValue, invoiceEntry.vatValue) &&
+        Objects.equals(grossValue, invoiceEntry.grossValue) &&
+        vatRate == invoiceEntry.vatRate;
+  }
 
   @Override
   public int hashCode() {
     return Objects.hash(id, item, quantity, price, vatValue, grossValue, vatRate);
   }
 
-    @Override
-    public String toString() {
-        return String.format("id: %s, item: %s, quantity: %s, price: %s, vatValue: %s, grossValue: %s, vatRate: %s",
-                id, item, quantity, price, vatValue, grossValue, vatRate);
-    }
+  @Override
+  public String toString() {
+    return String.format("id: %s, item: %s, quantity: %s, price: %s, vatValue: %s, grossValue: %s, vatRate: %s",
+        id, item, quantity, price, vatValue, grossValue, vatRate);
+  }
 }
