1b5ecc40ab4c351b0616e0269fb4fbd63d3d4425
git@github.com:CodersTrustPL/solutions-8-maksym.git <chernevskyymaks@gmail.com>
2019-01-31 01:22:29 +0100

    14-Validators

--- src/test/java/pl/coderstrust/generators/InvoiceEntriesGenerator.java
+++ src/test/java/pl/coderstrust/generators/InvoiceEntriesGenerator.java
@@ -2,6 +2,7 @@ package pl.coderstrust.generators;
 
 import java.math.BigDecimal;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicLong;
 
 import pl.coderstrust.model.InvoiceEntry;
@@ -14,7 +15,7 @@ public class InvoiceEntriesGenerator {
   public static InvoiceEntry getRandomInvoiceEntry() {
     long id = atomicLong.incrementAndGet();
     String item = WordGenerator.getRandomWord();
-    Long quantity = (long) random.nextInt(10000);
+    Long quantity = ThreadLocalRandom.current().nextLong(1, 999);
     BigDecimal price = BigDecimal.valueOf(random.nextInt(2000) * quantity);
     BigDecimal vatValue = price.multiply(BigDecimal.valueOf(Vat.VAT_23.getValue())).setScale(2, BigDecimal.ROUND_HALF_EVEN);
     BigDecimal grossValue = price.add(vatValue);
