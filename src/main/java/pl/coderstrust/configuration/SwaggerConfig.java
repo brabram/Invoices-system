7c73a8d2a70316cf0c91bfdee390fe7bb831186a
BrabaraM <barbara.mrugalska@gmail.com>
2019-02-05 17:25:01 +0100

    #22 add swagger to project. changes in SwaggerConfig

--- src/main/java/pl/coderstrust/configuration/SwaggerConfig.java
+++ src/main/java/pl/coderstrust/configuration/SwaggerConfig.java
@@ -7,6 +7,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -30,8 +31,9 @@ public class SwaggerConfig {
   private ApiInfo apiInfo() {
     return new ApiInfoBuilder()
         .title("Invoice API")
-        .description("Application to add, update, delete and get invoices")
-        .version("1.0")
+        .description("This panel allow you to test all possibilities of Invoice API.")
+        .version("1.0.0")
+        .contact(new Contact("Group 8, team B", "http://teamb.com", "teamb@gmail.com"))
         .build();
   }
 }
