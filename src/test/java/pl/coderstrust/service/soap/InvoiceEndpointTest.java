19414fe09212f782120c62b32e88d28f8d748a4a
BrabaraM <barbara.mrugalska@gmail.com>
2019-02-17 22:44:51 +0100

    #25 SOAP. added addInvoice and getAllInvoices test methods

---
+++ src/test/java/pl/coderstrust/service/soap/InvoiceEndpointTest.java
@@ -0,0 +1,80 @@
+package pl.coderstrust.service.soap;
+
+import static org.springframework.ws.test.server.RequestCreators.withPayload;
+import static org.springframework.ws.test.server.ResponseMatchers.noFault;
+import static org.springframework.ws.test.server.ResponseMatchers.payload;
+import static org.springframework.ws.test.server.ResponseMatchers.validPayload;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import javax.xml.transform.Source;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.ws.test.server.MockWebServiceClient;
+import org.springframework.xml.transform.StringSource;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class InvoiceEndpointTest {
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+  private MockWebServiceClient mockWebServiceClient;
+  private Resource xsdSchema = new ClassPathResource("invoicesSchema.xsd");
+
+  @BeforeEach
+  public void setup() {
+    mockWebServiceClient = MockWebServiceClient.createClient(webApplicationContext);
+  }
+
+  @Test
+  void shouldAddInvoiceResponse() throws IOException {
+    //Given
+    String filePathRequest = "src/test/resources/soap/addInvoiceRequest.xml";
+    String filePathResponse = "src/test/resources/soap/addInvoiceResponse.xml";
+    String request = Files.lines(Paths.get(filePathRequest)).collect(Collectors.joining("\n"));
+    String response = Files.lines(Paths.get(filePathResponse)).collect(Collectors.joining("\n"));
+    Source requestPayload = new StringSource(request);
+    Source responsePayload = new StringSource(response);
+
+    //When
+    mockWebServiceClient
+        .sendRequest(withPayload(requestPayload))
+        //Then
+        .andExpect(noFault())
+        .andExpect(payload(responsePayload))
+        .andExpect(validPayload(xsdSchema));
+
+  }
+
+  @Test
+  void shouldGetAllInvoiceResponse() throws IOException {
+    //Given
+    String filePathRequest = "src/test/resources/soap/getAllInvoicesRequest.xml";
+    String filePathResponse = "src/test/resources/soap/getAllInvoicesResponse.xml";
+    String request = Files.lines(Paths.get(filePathRequest)).collect(Collectors.joining("\n"));
+    String response = Files.lines(Paths.get(filePathResponse)).collect(Collectors.joining("\n"));
+    Source requestPayload = new StringSource(request);
+    Source responsePayload = new StringSource(response);
+
+    //When
+    mockWebServiceClient
+        .sendRequest(withPayload(requestPayload))
+        //Then
+        .andExpect(noFault())
+        .andExpect(payload(responsePayload))
+        .andExpect(validPayload(xsdSchema));
+
+  }
+}
