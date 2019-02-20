b8615c5ad45f1366b829990acde657e541a86e89
BrabaraM <barbara.mrugalska@gmail.com>
2019-02-20 18:12:46 +0100

    #25 SOAP. changes in InvoiceEndpoint. Added status message and status to responses.

--- src/main/java/pl/coderstrust/service/soap/InvoiceEndpoint.java
+++ src/main/java/pl/coderstrust/service/soap/InvoiceEndpoint.java
@@ -5,17 +5,17 @@ import static pl.coderstrust.service.soap.InvoiceToXmlConverter.convertInvoiceTo
 
 import java.util.List;
 import java.util.Optional;
-import javax.xml.datatype.DatatypeConfigurationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import pl.coderstrust.model.Invoice;
+import pl.coderstrust.model.validators.InvoiceValidator;
 import pl.coderstrust.service.rest.InvoiceService;
-import pl.coderstrust.service.rest.ServiceOperationException;
 import pl.coderstrust.soap.domainclasses.AddInvoiceResponse;
 import pl.coderstrust.soap.domainclasses.InvoicesList;
+import pl.coderstrust.soap.domainclasses.Status;
 
 @Endpoint
 public class InvoiceEndpoint {
@@ -30,81 +30,167 @@ public class InvoiceEndpoint {
 
   @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getAllInvoicesRequest")
   @ResponsePayload
-  public pl.coderstrust.soap.domainclasses.GetAllInvoicesResponse getAllInvoices(@RequestPayload pl.coderstrust.soap.domainclasses.GetAllInvoicesRequest request) throws ServiceOperationException, DatatypeConfigurationException {
+  public pl.coderstrust.soap.domainclasses.GetAllInvoicesResponse getAllInvoices(@RequestPayload pl.coderstrust.soap.domainclasses.GetAllInvoicesRequest request) {
     pl.coderstrust.soap.domainclasses.GetAllInvoicesResponse response = new pl.coderstrust.soap.domainclasses.GetAllInvoicesResponse();
-    Optional<List<Invoice>> invoicesOptionalList = invoiceService.getAllInvoices();
-    if (invoicesOptionalList.isPresent()) {
-      InvoicesList invoices = new InvoicesList();
-      for (Invoice invoice : invoicesOptionalList.get()) {
-        invoices.getInvoice().add(convertInvoiceToXml(invoice));
+    try {
+      Optional<List<Invoice>> invoicesOptionalList = invoiceService.getAllInvoices();
+      if (invoicesOptionalList.isPresent()) {
+        InvoicesList invoices = new InvoicesList();
+        for (Invoice invoice : invoicesOptionalList.get()) {
+          invoices.getInvoice().add(convertInvoiceToXml(invoice));
+        }
+        response.setStatus(Status.SUCCESS);
+        response.setStatusMessage("All invoices were downloaded.");
+        response.setInvoicesList(invoices);
+        return response;
       }
-      response.setInvoicesList(invoices);
+    } catch (Exception e) {
+      response.setStatus(Status.ERROR);
+      response.setStatusMessage("An error while getting invoices from database.");
     }
     return response;
   }
 
   @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getInvoiceByIdRequest")
   @ResponsePayload
-  public pl.coderstrust.soap.domainclasses.GetInvoiceByIdResponse getInvoiceById(@RequestPayload pl.coderstrust.soap.domainclasses.GetInvoiceByIdRequest request) throws ServiceOperationException, DatatypeConfigurationException {
+  public pl.coderstrust.soap.domainclasses.GetInvoiceByIdResponse getInvoiceById(@RequestPayload pl.coderstrust.soap.domainclasses.GetInvoiceByIdRequest request) {
     pl.coderstrust.soap.domainclasses.GetInvoiceByIdResponse response = new pl.coderstrust.soap.domainclasses.GetInvoiceByIdResponse();
-    Optional<Invoice> invoiceOptional = invoiceService.getInvoiceById(request.getId());
-    if (invoiceOptional.isPresent()) {
-      response.setInvoice(convertInvoiceToXml(invoiceOptional.get()));
+    long id = request.getId();
+    try {
+      Optional<Invoice> invoiceOptional = invoiceService.getInvoiceById(id);
+      if (invoiceOptional.isPresent()) {
+        response.setStatus(Status.SUCCESS);
+        response.setStatusMessage(String.format("Invoice with id %d were downloaded", id));
+        response.setInvoice(convertInvoiceToXml(invoiceOptional.get()));
+        return response;
+      }
+      response.setStatus(Status.ERROR);
+      response.setStatusMessage(String.format("Not found invoice with id %d", id));
+      return response;
+    } catch (Exception e) {
+      response.setStatus(Status.ERROR);
+      response.setStatusMessage(String.format("An error while getting invoice with id %d from database.", id));
     }
     return response;
   }
 
   @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getInvoiceByNumberRequest")
   @ResponsePayload
-  public pl.coderstrust.soap.domainclasses.GetInvoiceByNumberResponse getInvoiceByNumber(@RequestPayload pl.coderstrust.soap.domainclasses.GetInvoiceByNumberRequest request) throws ServiceOperationException, DatatypeConfigurationException {
+  public pl.coderstrust.soap.domainclasses.GetInvoiceByNumberResponse getInvoiceByNumber(@RequestPayload pl.coderstrust.soap.domainclasses.GetInvoiceByNumberRequest request) {
     pl.coderstrust.soap.domainclasses.GetInvoiceByNumberResponse response = new pl.coderstrust.soap.domainclasses.GetInvoiceByNumberResponse();
-    Optional<List<Invoice>> optionalInvoicesList = invoiceService.getAllInvoices();
-    if (optionalInvoicesList.isPresent()) {
-      Optional<Invoice> optionalInvoice = optionalInvoicesList.get()
-          .stream()
-          .filter(invoiceToGet -> invoiceToGet.getNumber().equals(request.getNumber()))
-          .findFirst();
-      if (optionalInvoice.isPresent()) {
-        response.setInvoice(convertInvoiceToXml(optionalInvoice.get()));
+    String number = request.getNumber();
+    try {
+      Optional<List<Invoice>> optionalInvoicesList = invoiceService.getAllInvoices();
+      if (optionalInvoicesList.isPresent()) {
+        Optional<Invoice> optionalInvoice = optionalInvoicesList.get()
+            .stream()
+            .filter(invoiceToGet -> invoiceToGet.getNumber().equals(number))
+            .findFirst();
+        if (optionalInvoice.isPresent()) {
+          response.setStatus(Status.SUCCESS);
+          response.setStatusMessage(String.format("Invoice with number %s were downloaded", number));
+          response.setInvoice(convertInvoiceToXml(optionalInvoice.get()));
+          return response;
+        }
+        response.setStatus(Status.ERROR);
+        response.setStatusMessage(String.format("Not found invoice with number %s", number));
+        return response;
       }
+    } catch (Exception e) {
+      response.setStatus(Status.ERROR);
+      response.setStatusMessage(String.format("An error while getting invoice with number %s from database.", number));
     }
     return response;
   }
 
   @PayloadRoot(namespace = NAMESPACE_URI, localPart = "addInvoiceRequest")
   @ResponsePayload
-  public pl.coderstrust.soap.domainclasses.AddInvoiceResponse addInvoice(@RequestPayload pl.coderstrust.soap.domainclasses.AddInvoiceRequest request) throws ServiceOperationException, DatatypeConfigurationException {
+  public pl.coderstrust.soap.domainclasses.AddInvoiceResponse addInvoice(@RequestPayload pl.coderstrust.soap.domainclasses.AddInvoiceRequest request) {
     pl.coderstrust.soap.domainclasses.AddInvoiceResponse response = new AddInvoiceResponse();
-    Invoice invoice = convertInvoiceFromXml(request.getInvoice());
-    Optional<Invoice> optionalInvoice = invoiceService.addInvoice(invoice);
-    if (optionalInvoice.isPresent()) {
-      response.setInvoice(convertInvoiceToXml(optionalInvoice.get()));
+    try {
+      Invoice invoice = convertInvoiceFromXml(request.getInvoice());
+      List<String> resultOfValidation = InvoiceValidator.validate(invoice, false);
+      if (resultOfValidation.size() > 0) {
+        response.setStatus(Status.ERROR);
+        response.setStatusMessage(String.format("Passed invoice is invalid. %s", resultOfValidation.toString()));
+        return response;
+      }
+      if (invoice.getId() == null || !invoiceService.invoiceExistsById(invoice.getId())) {
+        Optional<Invoice> optionalInvoice = invoiceService.addInvoice(invoice);
+        if (optionalInvoice.isPresent()) {
+          response.setStatus(Status.SUCCESS);
+          response.setStatusMessage(String.format("Added new invoice with id %d.", optionalInvoice.get().getId()));
+          response.setInvoice(convertInvoiceToXml(optionalInvoice.get()));
+          return response;
+        }
+        response.setStatus(Status.ERROR);
+        response.setStatusMessage("An error while adding invoice ");
+        return response;
+      }
+      response.setStatus(Status.ERROR);
+      response.setStatusMessage("Invoice already exist.");
+      return response;
+    } catch (Exception e) {
+      response.setStatus(Status.ERROR);
+      response.setStatusMessage("An error while adding invoice ");
     }
     return response;
   }
 
   @PayloadRoot(namespace = NAMESPACE_URI, localPart = "updateInvoiceRequest")
   @ResponsePayload
-  public pl.coderstrust.soap.domainclasses.UpdateInvoiceResponse updateInvoice(@RequestPayload pl.coderstrust.soap.domainclasses.UpdateInvoiceRequest request) throws ServiceOperationException, DatatypeConfigurationException {
+  public pl.coderstrust.soap.domainclasses.UpdateInvoiceResponse updateInvoice(@RequestPayload pl.coderstrust.soap.domainclasses.UpdateInvoiceRequest request) {
     pl.coderstrust.soap.domainclasses.UpdateInvoiceResponse response = new pl.coderstrust.soap.domainclasses.UpdateInvoiceResponse();
-    invoiceService.updateInvoice(convertInvoiceFromXml(request.getInvoice()));
-    Optional<Invoice> optionalInvoice = invoiceService.getInvoiceById(request.getId());
-    if (optionalInvoice.isPresent()) {
-      response.setInvoice(convertInvoiceToXml(optionalInvoice.get()));
+    try {
+      long id = request.getId();
+      Invoice invoice = convertInvoiceFromXml(request.getInvoice());
+      List<String> resultOfValidation = InvoiceValidator.validate(invoice, true);
+      if (resultOfValidation.size() > 0) {
+        response.setStatus(Status.ERROR);
+        response.setStatusMessage(String.format("Passed invoice is invalid. %s", resultOfValidation.toString()));
+        return response;
+      }
+      if (id != invoice.getId()) {
+        response.setStatus(Status.ERROR);
+        response.setStatusMessage(String.format("Invoice to update has different id than %d.", id));
+        return response;
+      }
+      if (!invoiceService.invoiceExistsById(id)) {
+        response.setStatus(Status.ERROR);
+        response.setStatusMessage(String.format("Not found invoice with id %d", id));
+        return response;
+      }
+      invoiceService.updateInvoice(invoice);
+      response.setStatus(Status.SUCCESS);
+      response.setInvoice(convertInvoiceToXml(invoice));
+      return response;
+    } catch (Exception e) {
+      response.setStatus(Status.ERROR);
+      response.setStatusMessage("An error while updating invoice ");
     }
     return response;
   }
 
   @PayloadRoot(namespace = NAMESPACE_URI, localPart = "deleteInvoiceRequest")
   @ResponsePayload
-  public pl.coderstrust.soap.domainclasses.DeleteInvoiceResponse deleteInvoice(@RequestPayload pl.coderstrust.soap.domainclasses.DeleteInvoiceRequest request) throws ServiceOperationException, DatatypeConfigurationException {
+  public pl.coderstrust.soap.domainclasses.DeleteInvoiceResponse deleteInvoice(@RequestPayload pl.coderstrust.soap.domainclasses.DeleteInvoiceRequest request) {
     pl.coderstrust.soap.domainclasses.DeleteInvoiceResponse response = new pl.coderstrust.soap.domainclasses.DeleteInvoiceResponse();
-    invoiceService.deleteInvoiceById(request.getId());
-    Optional<Invoice> optionalInvoice = invoiceService.getInvoiceById(request.getId());
-    if (!optionalInvoice.isPresent()) {
-      response.setStatusMessage(String.format("Invoice with %d id removed", request.getId()));
+    long id = request.getId();
+    try {
+      Optional<Invoice> optionalInvoice = invoiceService.getInvoiceById(id);
+      if (optionalInvoice.isPresent()) {
+        invoiceService.deleteInvoiceById(id);
+        response.setStatus(Status.SUCCESS);
+        response.setStatusMessage(String.format("Invoice with id %d was removed", id));
+        return response;
+      }
+      response.setStatus(Status.ERROR);
+      response.setStatusMessage(String.format("Invoice with id %d does not exist.", id));
+      return response;
+    } catch (Exception e) {
+      response.setStatus(Status.ERROR);
+      response.setStatusMessage("An error while deleting invoice ");
     }
     return response;
   }
-
 }
