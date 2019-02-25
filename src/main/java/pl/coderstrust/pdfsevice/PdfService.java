86db6aa39a26e3a3295045856e3b2c0ae60ea2ca
git@github.com:CodersTrustPL/solutions-8-maksym.git <chernevskyymaks@gmail.com>
2019-02-25 22:32:13 +0100

    26-pdfService

--- src/main/java/pl/coderstrust/pdfsevice/PdfService.java
+++ src/main/java/pl/coderstrust/pdfsevice/PdfService.java
@@ -5,7 +5,6 @@ import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Phrase;
-import com.itextpdf.text.pdf.PdfChunk;
 import com.itextpdf.text.pdf.PdfPCell;
 import java.io.ByteArrayOutputStream;
 import com.itextpdf.text.Document;
@@ -34,31 +33,44 @@ public class PdfService {
         FontFactory.getFont(FontFactory.HELVETICA, 12, Font.BOLD));
     title.setAlignment(title.ALIGN_RIGHT);
     title.add(String.valueOf("/ID" + invoice.getId()));
-    Paragraph issueDate = new Paragraph(String.valueOf(invoice.getIssueDate() + " - " + invoice.getDueDate()));
+    Paragraph issueDate = new Paragraph(String.valueOf("Issue date: " + invoice.getIssueDate()));
     issueDate.setAlignment(issueDate.ALIGN_RIGHT);
+    Paragraph dueDate = new Paragraph(String.valueOf("Due date: " + invoice.getDueDate()));
+    issueDate.setAlignment(issueDate.ALIGN_RIGHT);
+    dueDate.setAlignment(dueDate.ALIGN_RIGHT);
     document.add(title);
     document.add(issueDate);
+    document.add(dueDate);
     PdfPTable table = new PdfPTable(2);
     PdfPCell seller = new PdfPCell();
-    seller.addElement(companySeller(invoice.getSeller()));
+    Paragraph sellerTitle = new Paragraph("Seller: ",
+        FontFactory.getFont(FontFactory.HELVETICA_BOLD, 12));
+    seller.addElement(sellerTitle);
+    seller.addElement(company(invoice.getSeller()));
     seller.setBorder(seller.NO_BORDER);
     PdfPCell buyer = new PdfPCell();
+    Paragraph buyerTitle = new Paragraph("Buyer: ",
+        FontFactory.getFont(FontFactory.HELVETICA_BOLD, 12));
     buyer.setBorder(buyer.NO_BORDER);
-    buyer.addElement(companyBuyer(invoice.getBuyer()));
+    buyer.addElement(buyerTitle);
+    buyer.addElement(company(invoice.getBuyer()));
     table.setSpacingBefore(10);
     table.setWidthPercentage(100);
     table.addCell(seller);
     table.addCell(buyer);
     document.add(table);
-    paragraph.add(String.valueOf(invoice.getTotalGrossValue()));
     document.add(listInvoiceEntries(invoice.getEntries()));
+    Paragraph totalNetValue = new Paragraph("Total net value: " + invoice.getTotalNetValue());
+    Paragraph totalGrossValue = new Paragraph("Total gross value: " + invoice.getTotalGrossValue());
+    paragraph.add(totalNetValue);
+    paragraph.add(totalGrossValue);
+    document.add(paragraph);
     document.close();
     return stream.toByteArray();
   }
 
   private PdfPTable listInvoiceEntries(List<InvoiceEntry> invoiceEntries) throws DocumentException {
-    String[] tableHeaders = new String[]{"a", "b", "c", "d", "e", "F", "g"};
-
+    String[] tableHeaders = new String[] {"Id", "Item", "Quantity", "Price", "Vat Value", "Gross value", "Vat rate"};
     PdfPTable table = new PdfPTable(7);
     table.setSpacingBefore(25);
     table.setSpacingAfter(25);
@@ -67,7 +79,7 @@ public class PdfService {
     for (InvoiceEntry invoiceEntry : invoiceEntries) {
       table.addCell(invoiceEntry.getId().toString());
       table.addCell(invoiceEntry.getItem());
-      table.addCell(invoiceEntry.getPrice().toString());
+      table.addCell(invoiceEntry.getQuantity().toString());
       table.addCell(invoiceEntry.getPrice().toString());
       table.addCell(invoiceEntry.getVatValue().toString());
       table.addCell(invoiceEntry.getGrossValue().toString());
@@ -76,82 +88,28 @@ public class PdfService {
     return table;
   }
 
-  private void addInvoiceEntriestableHeander(PdfPTable table, String[] headers){
-    for(String heander: headers){
-      PdfPCell header = new PdfPCell();
-      header.setBackgroundColor(BaseColor.GRAY);
-      header.setBorderWidth(2);
-      header.setPhrase(new Phrase(heander));
-      table.addCell(header);
+  private void addInvoiceEntriestableHeander(PdfPTable table, String[] headers) {
+    for (String heander : headers) {
+      PdfPCell title = new PdfPCell();
+      title.setBackgroundColor(BaseColor.GRAY);
+      title.setHorizontalAlignment(title.ALIGN_CENTER);
+      title.setVerticalAlignment(title.ALIGN_CENTER);
+      title.setBorderWidth(2);
+      title.setPhrase(new Phrase(heander));
+      table.addCell(title);
     }
   }
 
-  private Paragraph invoiceEntry(InvoiceEntry invoiceEntry){
-//     PdfPCell cell = new PdfPCell();
-//    PdfPCell id = new PdfPCell(new Paragraph(invoiceEntry.getId()));
-//    PdfPCell item = new PdfPCell(new Paragraph(invoiceEntry.getItem()));
-//    PdfPCell quantity = new PdfPCell(new Paragraph(invoiceEntry.getQuantity()));
-//    PdfPCell price = new PdfPCell(new Paragraph(String.valueOf(invoiceEntry.getPrice())));
-//    PdfPCell vatValue = new PdfPCell(new Paragraph(String.valueOf(invoiceEntry.getVatValue())));
-//    PdfPCell grossValue = new PdfPCell(new Paragraph(String.valueOf(invoiceEntry.getGrossValue())));
-//    PdfPCell vatRate = new PdfPCell(new Paragraph(String.valueOf(invoiceEntry.getVatRate())));
-//    cell.addElement(id);
-//    cell.addElement(item);
-//    cell.addElement(quantity);
-//    cell.addElement(price);
-//    cell.addElement(vatValue);
-//    cell.addElement(grossValue);
-//    cell.addElement(vatRate);
-
-    Paragraph id = new Paragraph(invoiceEntry.getId());
-    Paragraph item = new Paragraph(invoiceEntry.getItem());
-    Paragraph quantity = new Paragraph(invoiceEntry.getQuantity());
-    Paragraph price = new Paragraph(String.valueOf(invoiceEntry.getPrice()));
-    Paragraph vatValue = new Paragraph(String.valueOf(invoiceEntry.getVatValue()));
-    Paragraph grossValue = new Paragraph(String.valueOf(invoiceEntry.getGrossValue()));
-    Paragraph vatRate = new Paragraph(String.valueOf(invoiceEntry.getVatRate()));
-    Paragraph paragraph = new Paragraph();
-    paragraph.add(id);
-    paragraph.add(item);
-    paragraph.add(quantity);
-    paragraph.add(price);
-    paragraph.add(vatValue);
-    paragraph.add(grossValue);
-    paragraph.add(vatRate);
-    return paragraph;
-  }
-
-  private Paragraph companySeller(Company company) {
-    Paragraph seller = new Paragraph("Seller: ",
-        FontFactory.getFont(FontFactory.HELVETICA_BOLD, 12));
+  private Paragraph company(Company company) {
     Paragraph paragraph = new Paragraph();
     Paragraph name = new Paragraph(company.getName());
     Paragraph id = new Paragraph(company.getId());
     Paragraph taxIdentificationNumber = new Paragraph("Tax id : " + company.getTaxIdentificationNumber());
-    paragraph.add(seller);
     paragraph.add(name);
     paragraph.add(id);
     paragraph.add(taxIdentificationNumber);
     paragraph.add(contactDetails(company.getContactDetails()));
     paragraph.add(accountNumber(company.getAccountNumber()));
-    return paragraph;
-  }
-
-  private Paragraph companyBuyer(Company company) {
-    Paragraph buyer = new Paragraph("Buyer: ",
-        FontFactory.getFont(FontFactory.HELVETICA_BOLD, 12));
-    Paragraph name = new Paragraph(company.getName());
-    Paragraph id = new Paragraph(company.getId());
-    Paragraph taxIdentificationNumber = new Paragraph("Tax id : " + company.getTaxIdentificationNumber());
-    Paragraph paragraph = new Paragraph();
-    paragraph.add(buyer);
-    paragraph.add(name);
-    paragraph.add(id);
-    paragraph.add(taxIdentificationNumber);
-    Paragraph contactDetails = contactDetails(company.getContactDetails());
-    paragraph.add(contactDetails);
-    Paragraph accountNumber = accountNumber(company.getAccountNumber());
-    paragraph.add(accountNumber);
     return paragraph;
   }
 
