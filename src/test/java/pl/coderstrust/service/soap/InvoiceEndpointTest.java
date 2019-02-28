0105e9304d0d5f6dc33103220465422aaf7a032a
BrabaraM <barbara.mrugalska@gmail.com>
2019-02-28 21:12:02 +0100

    #22 soap. added more tests to add, update and delete method

--- src/test/java/pl/coderstrust/service/soap/InvoiceEndpointTest.java
+++ src/test/java/pl/coderstrust/service/soap/InvoiceEndpointTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,6 +19,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.coderstrust.generators.InvoiceGenerator;
 import pl.coderstrust.model.Invoice;
+import pl.coderstrust.model.validators.InvoiceValidator;
 import pl.coderstrust.service.rest.InvoiceService;
 import pl.coderstrust.service.rest.ServiceOperationException;
 import pl.coderstrust.soap.domainclasses.AddInvoiceRequest;
@@ -35,7 +37,7 @@ import pl.coderstrust.soap.domainclasses.UpdateInvoiceRequest;
 import pl.coderstrust.soap.domainclasses.UpdateInvoiceResponse;
 
 @ExtendWith(MockitoExtension.class)
-public class InvoiceEndopointTest {
+public class InvoiceEndpointTest {
 
   @Mock
   InvoiceService invoiceService;
@@ -103,7 +105,7 @@ public class InvoiceEndopointTest {
   }
 
   @Test
-  void getInvoiceByIdMethodShouldReturnErrorWhenInvoiceWithSpecificIdDoesNotExist() throws ServiceOperationException, DatatypeConfigurationException {
+  void getInvoiceByIdMethodShouldReturnErrorWhenInvoiceWithSpecificIdDoesNotExist() throws ServiceOperationException {
     //Given
     Invoice expectedInvoice = InvoiceGenerator.getRandomInvoice();
     Long id = expectedInvoice.getId() + 1L;
@@ -124,7 +126,7 @@ public class InvoiceEndopointTest {
   }
 
   @Test
-  void getInvoiceByIdMethodShouldReturnErrorOccurDuringExecutionGettingInvoicesFromDatabase() throws ServiceOperationException {
+  void getInvoiceByIdMethodShouldReturnErrorOccurDuringExecutionGettingInvoiceByIdFromDatabase() throws ServiceOperationException {
     //Given
     GetInvoiceByIdRequest request = new GetInvoiceByIdRequest();
     long id = 1L;
@@ -170,7 +172,7 @@ public class InvoiceEndopointTest {
   }
 
   @Test
-  void getInvoiceByNumberMethodShouldReturnErrorWhenInvoiceWithSpecificIdDoesNotExist() throws ServiceOperationException, DatatypeConfigurationException {
+  void getInvoiceByNumberMethodShouldReturnErrorWhenInvoiceWithSpecificIdDoesNotExist() throws ServiceOperationException {
     //Given
     Invoice expectedInvoice = InvoiceGenerator.getRandomInvoice();
     Invoice expectedInvoice2 = InvoiceGenerator.getRandomInvoice();
@@ -239,6 +241,70 @@ public class InvoiceEndopointTest {
   }
 
   @Test
+  void addInvoiceMethodShouldReturnErrorWhenPassedInvoiceIsInvalid() throws DatatypeConfigurationException {
+    //Given
+    Invoice expectedInvoice = InvoiceGenerator.getRandomInvoice();
+    expectedInvoice.setIssueDate(LocalDate.now());
+    expectedInvoice.setDueDate(LocalDate.of(2019,01,01));
+    List<String> resultOfValidation = InvoiceValidator.validate(expectedInvoice, false);
+    AddInvoiceRequest request = new AddInvoiceRequest();
+    request.setInvoice(InvoiceToXmlConverter.convertInvoiceToXml(expectedInvoice));
+    String expectedMessage = String.format("Passed invoice is invalid. %s.", resultOfValidation.toString());
+
+    //When
+    AddInvoiceResponse response = invoiceEndpoint.addInvoice(request);
+    Status actualStatus = response.getStatus();
+    String actualMessage = response.getStatusMessage();
+
+    //Then
+    assertEquals(Status.ERROR, actualStatus);
+    assertEquals(expectedMessage, actualMessage);
+  }
+
+  @Test
+  void addInvoiceMethodShouldReturnErrorWhenPassedInvoiceAlreadyExist() throws ServiceOperationException, DatatypeConfigurationException {
+    //Given
+    Invoice expectedInvoice = InvoiceGenerator.getRandomInvoice();
+    long id = expectedInvoice.getId();
+    when(invoiceService.invoiceExistsById(id)).thenReturn(true);
+    AddInvoiceRequest request = new AddInvoiceRequest();
+    request.setInvoice(InvoiceToXmlConverter.convertInvoiceToXml(expectedInvoice));
+    String expectedMessage = "Invoice already exist.";
+
+    //When
+    AddInvoiceResponse response = invoiceEndpoint.addInvoice(request);
+    Status actualStatus = response.getStatus();
+    String actualMessage = response.getStatusMessage();
+
+    //Then
+    assertEquals(Status.ERROR, actualStatus);
+    assertEquals(expectedMessage, actualMessage);
+    verify(invoiceService).invoiceExistsById(id);
+  }
+
+  @Test
+  void addInvoiceMethodShouldReturnErrorOccurDuringExecutionAddingInvoiceToDatabase() throws ServiceOperationException, DatatypeConfigurationException {
+    //Given
+    Invoice expectedInvoice = InvoiceGenerator.getRandomInvoice();
+    long id = expectedInvoice.getId();
+    when(invoiceService.invoiceExistsById(id)).thenReturn(false);
+    doThrow(ServiceOperationException.class).when(invoiceService).addInvoice(expectedInvoice);
+    AddInvoiceRequest request = new AddInvoiceRequest();
+    request.setInvoice(InvoiceToXmlConverter.convertInvoiceToXml(expectedInvoice));
+    String expectedMessage = "An error while adding invoice.";
+
+    //When
+    AddInvoiceResponse response = invoiceEndpoint.addInvoice(request);
+    Status actualStatus = response.getStatus();
+    String actualMessage = response.getStatusMessage();
+
+    //Then
+    assertEquals(Status.ERROR, actualStatus);
+    assertEquals(expectedMessage, actualMessage);
+    verify(invoiceService).invoiceExistsById(id);
+  }
+
+  @Test
   void shouldUpdateInvoice() throws ServiceOperationException, DatatypeConfigurationException {
     //Given
     Invoice expectedInvoice = InvoiceGenerator.getRandomInvoice();
@@ -265,7 +331,93 @@ public class InvoiceEndopointTest {
   }
 
   @Test
-  void shouldDeleteInvoice() throws ServiceOperationException, DatatypeConfigurationException {
+  void updateInvoiceMethodShouldReturnErrorWhenPassedInvoiceIsInvalid() throws DatatypeConfigurationException {
+    //Given
+    Invoice expectedInvoice = InvoiceGenerator.getRandomInvoice();
+    expectedInvoice.setIssueDate(LocalDate.now());
+    expectedInvoice.setDueDate(LocalDate.of(2019,01,01));
+    List<String> resultOfValidation = InvoiceValidator.validate(expectedInvoice, false);
+    UpdateInvoiceRequest request = new UpdateInvoiceRequest();
+    request.setInvoice(InvoiceToXmlConverter.convertInvoiceToXml(expectedInvoice));
+    String expectedMessage = String.format("Passed invoice is invalid. %s.", resultOfValidation.toString());
+
+    //When
+    UpdateInvoiceResponse response = invoiceEndpoint.updateInvoice(request);
+    Status actualStatus = response.getStatus();
+    String actualMessage = response.getStatusMessage();
+
+    //Then
+    assertEquals(Status.ERROR, actualStatus);
+    assertEquals(expectedMessage, actualMessage);
+  }
+
+  @Test
+  void updateInvoiceMethodShouldReturnErrorWhenPassedInvoiceDoesNotExist() throws ServiceOperationException, DatatypeConfigurationException {
+    //Given
+    Invoice expectedInvoice = InvoiceGenerator.getRandomInvoice();
+    long id = expectedInvoice.getId();
+    when(invoiceService.invoiceExistsById(id)).thenReturn(false);
+    UpdateInvoiceRequest request = new UpdateInvoiceRequest();
+    request.setInvoice(InvoiceToXmlConverter.convertInvoiceToXml(expectedInvoice));
+    request.setId(id);
+    String expectedMessage = String.format("Not found invoice with id %d.", id);
+
+    //When
+    UpdateInvoiceResponse response = invoiceEndpoint.updateInvoice(request);
+    Status actualStatus = response.getStatus();
+    String actualMessage = response.getStatusMessage();
+
+    //Then
+    assertEquals(Status.ERROR, actualStatus);
+    assertEquals(expectedMessage, actualMessage);
+    verify(invoiceService).invoiceExistsById(id);
+  }
+
+  @Test
+  void updateInvoiceMethodShouldReturnErrorWhenPassedInvoiceHasDifferentIdThanRequestId() throws DatatypeConfigurationException {
+    //Given
+    Invoice expectedInvoice = InvoiceGenerator.getRandomInvoice();
+    long id = expectedInvoice.getId();
+    UpdateInvoiceRequest request = new UpdateInvoiceRequest();
+    request.setInvoice(InvoiceToXmlConverter.convertInvoiceToXml(expectedInvoice));
+    request.setId(id + 1L);
+    String expectedMessage = String.format("Invoice to update has different id than %d.", id + 1);
+
+    //When
+    UpdateInvoiceResponse response = invoiceEndpoint.updateInvoice(request);
+    Status actualStatus = response.getStatus();
+    String actualMessage = response.getStatusMessage();
+
+    //Then
+    assertEquals(Status.ERROR, actualStatus);
+    assertEquals(expectedMessage, actualMessage);
+  }
+
+  @Test
+  void updateInvoiceMethodShouldReturnErrorOccurDuringExecutionUpdatingInvoiceInDatabase() throws ServiceOperationException, DatatypeConfigurationException {
+    //Given
+    Invoice expectedInvoice = InvoiceGenerator.getRandomInvoice();
+    long id = expectedInvoice.getId();
+    when(invoiceService.invoiceExistsById(id)).thenReturn(true);
+    doThrow(ServiceOperationException.class).when(invoiceService).updateInvoice(expectedInvoice);
+    UpdateInvoiceRequest request = new UpdateInvoiceRequest();
+    request.setInvoice(InvoiceToXmlConverter.convertInvoiceToXml(expectedInvoice));
+    request.setId(id);
+    String expectedMessage = "An error while updating invoice.";
+
+    //When
+    UpdateInvoiceResponse response = invoiceEndpoint.updateInvoice(request);
+    Status actualStatus = response.getStatus();
+    String actualMessage = response.getStatusMessage();
+
+    //Then
+    assertEquals(Status.ERROR, actualStatus);
+    assertEquals(expectedMessage, actualMessage);
+    verify(invoiceService).invoiceExistsById(id);
+  }
+
+  @Test
+  void shouldDeleteInvoice() throws ServiceOperationException {
     //Given
     Invoice expectedInvoice = InvoiceGenerator.getRandomInvoice();
     Long id = expectedInvoice.getId();
@@ -282,6 +434,48 @@ public class InvoiceEndopointTest {
 
     //Then
     assertEquals(Status.SUCCESS, actualStatus);
+    assertEquals(expectedStatusMessage, actualStatusMessage);
+    verify(invoiceService).getInvoiceById(id);
+  }
+
+  @Test
+  void deleteInvoiceMethodShoudReturnErrorWhenInvoiceDoesNotExist() throws ServiceOperationException {
+    //Given
+    Long id = 1L;
+    when(invoiceService.getInvoiceById(id)).thenReturn(Optional.empty());
+    DeleteInvoiceRequest request = new DeleteInvoiceRequest();
+    request.setId(id);
+    String expectedStatusMessage = String.format("Invoice with id %d does not exist.", id);
+
+    //When
+    DeleteInvoiceResponse response = invoiceEndpoint.deleteInvoice(request);
+    Status actualStatus = response.getStatus();
+    String actualStatusMessage = response.getStatusMessage();
+
+    //Then
+    assertEquals(Status.ERROR, actualStatus);
+    assertEquals(expectedStatusMessage, actualStatusMessage);
+    verify(invoiceService).getInvoiceById(id);
+  }
+
+  @Test
+  void deleteInvoiceMethodShouldReturnErrorWhenInvoiceDoesNotExist() throws ServiceOperationException {
+    //Given
+    Invoice expectedInvoice = InvoiceGenerator.getRandomInvoice();
+    Long id = expectedInvoice.getId();
+    when(invoiceService.getInvoiceById(id)).thenReturn(Optional.of(expectedInvoice));
+    doThrow(ServiceOperationException.class).when(invoiceService).deleteInvoiceById(id);
+    DeleteInvoiceRequest request = new DeleteInvoiceRequest();
+    request.setId(id);
+    String expectedStatusMessage = "An error while deleting invoice.";
+
+    //When
+    DeleteInvoiceResponse response = invoiceEndpoint.deleteInvoice(request);
+    Status actualStatus = response.getStatus();
+    String actualStatusMessage = response.getStatusMessage();
+
+    //Then
+    assertEquals(Status.ERROR, actualStatus);
     assertEquals(expectedStatusMessage, actualStatusMessage);
     verify(invoiceService).getInvoiceById(id);
   }
