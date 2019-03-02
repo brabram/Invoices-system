ac456978eaab56808d27db36dcdd37b2c95c6dca
git@github.com:CodersTrustPL/solutions-8-maksym.git <chernevskyymaks@gmail.com>
2019-03-02 17:35:49 +0100

    Angular

--- src/main/java/pl/coderstrust/controller/InvoiceController.java
+++ src/main/java/pl/coderstrust/controller/InvoiceController.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ import pl.coderstrust.service.InvoiceService;
 @Api(value = "/invoices", description = "Available operations for invoice application", tags = {"Invoices"})
 @RestController
 @RequestMapping("/invoices")
+@CrossOrigin
 public class InvoiceController {
 
   private static Logger log = LoggerFactory.getLogger(InvoiceController.class);
---
+++ src/main/resources/public/hello.js
@@ -0,0 +1,10 @@
+angular.module('demo', [])
+.controller('Invoices',
+    function($scope, $http) {
+
+    $http.get('http://localhost:8080/invoices')
+    .then(
+         function(response) {
+            $scope.invoices = response.data;
+        });
+    });
