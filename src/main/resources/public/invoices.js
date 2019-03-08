dbcf028708372041f6f92f9a0bd45d03c9756431
git@github.com:CodersTrustPL/solutions-8-maksym.git <chernevskyymaks@gmail.com>
2019-03-08 02:11:23 +0100

    30-Angular

--- src/main/resources/public/invoices.js
+++ src/main/resources/public/invoices.js
@@ -17,22 +17,22 @@ app.controller('InvoicesController', function ($scope, $http, $window) {
                     });
         };
 
-         $scope.pdf = function(id){
-          $window.open('http://localhost:8080/invoices/pdf/' + id, '_blank');
+        $scope.get = function(id){
+                    $http.get('http://localhost:8080/invoices/' + id).
+                    then(function (response) {
+                               $http.get('http://localhost:8080/invoices').
+                                       then(function (response) {
+                                           $scope.Invoices = response.data;
+                                           $scope.empty = $scope.Invoices.length;
+                                       });
+                            });
                 };
 
-//$scope.IsVisible = false;
-//             $http.get('http://localhost:8080/invoices').
-//            $scope.details = function (response) {
-//                           $scope.IsVisible = $scope.IsVisible true;
-//            }
+         $scope.pdf = function(id){
+          $window.open('http://localhost:8080/invoices/pdf/' + id, '_blank');
+    };
 });
-//        app.controller('FindById', function ($scope, $http) {
-//        $http.get('http://localhost:8080/invoices').
-//        then(function(response) {
-//            $scope.invoices = response.data;
-//                   };
-//                    });
+
 
 
 
