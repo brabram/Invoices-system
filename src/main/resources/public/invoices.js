144e675a33bdcf2d934e029c225d3f69bff7eef7
git@github.com:CodersTrustPL/solutions-8-maksym.git <chernevskyymaks@gmail.com>
2019-03-10 20:48:41 +0100

    30-Angular_Fixes

--- src/main/resources/public/invoices.js
+++ src/main/resources/public/invoices.js
@@ -1,39 +1,28 @@
 var app = angular.module('Invoices', []);
 
 app.controller('InvoicesController', function ($scope, $http, $window) {
-    $http.get('http://localhost:8080/invoices').
-        then(function (response) {
+
+    getAllInvoices()
+
+    $scope.remove = function (id) {
+        $http.delete(getBaseApiAddress() + id).then(function () {
+            getAllInvoices()
+        });
+    };
+
+    $scope.pdf = function (id) {
+        $window.open(getBaseApiAddress() + 'pdf/' + id, '_blank');
+    };
+
+    function getAllInvoices()
+    {
+        $http.get(getBaseApiAddress()).then(function (response) {
             $scope.Invoices = response.data;
             $scope.empty = $scope.Invoices.length;
         });
-        $scope.remove = function(id){
-            $http.delete('http://localhost:8080/invoices/' + id).
-            then(function (response) {
-                       $http.get('http://localhost:8080/invoices').
-                               then(function (response) {
-                                   $scope.Invoices = response.data;
-                                   $scope.empty = $scope.Invoices.length;
-                               });
-                    });
-        };
-
-        $scope.get = function(id){
-                    $http.get('http://localhost:8080/invoices/' + id).
-                    then(function (response) {
-                               $http.get('http://localhost:8080/invoices').
-                                       then(function (response) {
-                                           $scope.Invoices = response.data;
-                                           $scope.empty = $scope.Invoices.length;
-                                       });
-                            });
-                };
-
-         $scope.pdf = function(id){
-          $window.open('http://localhost:8080/invoices/pdf/' + id, '_blank');
-    };
+    }
 });
 
-
-
-
-
+function getBaseApiAddress(){
+    return 'http://localhost:8080/invoices/'
+}
